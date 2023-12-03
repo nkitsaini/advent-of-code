471699fa2b7f9252fa8257151450548da17c24a2
Ankit Saini <nnkitsaini@gmail.com>
2023-12-03 10:42:41 +0530

    auto_commit

--- 2023/day3/first.py
+++ 2023/day3/first.py
@@ -68,7 +68,7 @@ def main():
         if digit != "" and adjacent:
             print(digit)
             # ans += int(digit)
-            for gear_i in gear_idx:
+            for gear_i in set(gear_idx):
                 GEAR_VALUES[gear_i].append(int(digit))
 
 
