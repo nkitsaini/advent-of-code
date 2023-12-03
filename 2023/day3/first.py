b28ebd99f9a8e6890fdafe2b1a55957a3fa55a59
Ankit Saini <nnkitsaini@gmail.com>
2023-12-03 10:28:32 +0530

    auto_commit

---
+++ 2023/day3/first.py
@@ -0,0 +1,53 @@
+import sys
+from pprint import pprint
+from collections import *
+import string
+from typing import *
+
+
+
+T = TypeVar('T')
+def dbg(val: T) -> T:
+    print('---', val, file=sys.stderr)
+    return val
+
+
+def main():
+    # Raw Text
+    content = sys.stdin.read()
+
+    # each line stripped
+    lines = [x.strip() for x in content.splitlines()]
+
+    # each block (splitted by empty line)
+    splits = content.split('\n\n')
+
+    ans = 0
+
+    # Solve
+
+
+    
+    # for idx, line in enumerate(lines):
+    #     loc_ans = 0
+        
+        
+                
+            
+        
+
+    
+
+
+    
+
+
+    print(ans)
+                 
+
+
+    
+    
+
+if __name__ == "__main__":
+    main()
