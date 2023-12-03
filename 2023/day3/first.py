2b2edf06fdf3abc4a466f4bec90907b06a543dd1
Ankit Saini <nnkitsaini@gmail.com>
2023-12-03 10:41:48 +0530

    auto_commit

--- 2023/day3/first.py
+++ 2023/day3/first.py
@@ -86,7 +86,7 @@ def main():
         if len(val) != 2:
             continue
         print(k, val)
-        a = 1
+        a = val[0] * val[1]
         for x in val:
             a *= x
         ans += a
