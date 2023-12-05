17908a36aa9868b3a0b1c4155a1dd4f986abb6f0
Ankit Saini <nnkitsaini@gmail.com>
2023-12-05 10:57:12 +0530

    auto_commit

--- 2023/day5/first.py
+++ 2023/day5/first.py
@@ -108,7 +108,7 @@ def main():
         maps = mappings[p][past]
         # print(maps, past, p)
         next_values = []
-        print("values", len(values))
+        # print("values", len(values), len())
         for value in values:
             found = False
             for start, rng in values:
