619468e21787a69198d016970b3dc743be7bb22d
Ankit Saini <nnkitsaini@gmail.com>
2023-12-05 10:43:24 +0530

    auto_commit

--- 2023/day5/first.py
+++ 2023/day5/first.py
@@ -72,6 +72,7 @@ def main():
         for value in values:
             found = False
             for dest, src, count in maps:
+                dest, src, count = int(dest), int(src), int(count)
                 print(value, dest, src, count)
                 if src + count > value:
                     # value = 13, src = 10, dest = 90, count = 7
