18f6a7e643efc9b6aebce382ea6eda90fafad5f7
Ankit Saini <nnkitsaini@gmail.com>
2023-12-06 10:05:38 +0530

    auto_commit

---
+++ 2023/day6/second.py
@@ -0,0 +1,59 @@
+import sys
+import re
+from pprint import pprint
+from collections import *
+import string
+from typing import *
+
+
+
+T = TypeVar('T')
+def dbg(val: T) -> T:
+    print('---', val, file=sys.stderr)
+    return val
+
+DIRECTION_NAME = {
+    "right": (0, 1),
+    "left": (0, -1),
+    "up": (-1, 0),
+    "down": (1, 0),
+}
+DIRECTION_NAME_REVERSED = {v: k for k, v in DIRECTION_NAME.items()}
+
+DIRECTIONS = list(DIRECTION_NAME_REVERSED.keys())
+DIRECTIONS_EDGE = [
+    *DIRECTIONS,
+    (1, -1),
+    (-1, 1),
+    (-1, -1),
+    (1, 1),
+]
+
+def main():
+    # Raw Text
+    content = sys.stdin.read()
+
+    # each line stripped
+    lines = [x.strip() for x in content.splitlines()]
+
+    # each block (splitted by empty line)
+    splits = content.split('\n\n')
+
+    ans = 0
+    array = []
+
+    for idx, line in enumerate(lines):
+        ...
+
+
+    print(array)
+    print(ans)
+    
+
+    
+def repl():
+    ...
+
+if __name__ == "__main__":
+    # repl()
+    main()
