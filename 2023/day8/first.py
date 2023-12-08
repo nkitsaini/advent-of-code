1901bd702e798e473397c9d11145af583941b5e7
Ankit Saini <nnkitsaini@gmail.com>
2023-12-08 10:49:45 +0530

    auto_commit

--- 2023/day8/first.py
+++ 2023/day8/first.py
@@ -9,6 +9,24 @@ import string
 from typing import *
 
 
+# Python Program to find the L.C.M. of two input number
+
+def compute_lcm(x, y):
+
+   # choose the greater number
+   if x > y:
+       greater = x
+   else:
+       greater = y
+
+   while(True):
+       if((greater % x == 0) and (greater % y == 0)):
+           lcm = greater
+           break
+       greater += 1
+
+   return lcm
+
 
 T = TypeVar('T')
 def dbg(val: T) -> T:
