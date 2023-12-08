655689fd2b878e479ee172b869ed46707b2fc4f5
Ankit Saini <nnkitsaini@gmail.com>
2023-12-08 10:41:53 +0530

    auto_commit

--- 2023/day8/second.py
+++ 2023/day8/second.py
@@ -74,15 +74,13 @@ def main():
         current = value
         for instruct in enumerate(itertools.repeat(instructions)):
             next_values = []
-            for current in values:
-                if instruct == "L":
-                    current = graph[current]['left']
-                elif instruct == "R":
-                    current = graph[current]['right']
-                else:
-                    raise Exception()
-                next_values.append(current)
-            values = next_values
+            if instruct == "L":
+                current = graph[current]['left']
+            elif instruct == "R":
+                current = graph[current]['right']
+            else:
+                raise Exception()
+            next_values.append(current)
             if current[2] == 'Z':
                 found = True
                 break
